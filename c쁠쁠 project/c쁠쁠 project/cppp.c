c7fb7855bcd0b41f39abb3a2dacb96b6620a45fd
eunjin <keunjin312@naver.com>
2023-05-17 16:00:04 +0900

    add sln file

---
+++ c쁠쁠 project/c쁠쁠 project/cppp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+	// 주석
+	/* 여러줄 쓰고 싶을때는
+	                    요렇게 */
+	printf("Hello World!\n"); //문장출력
+	int number; //변수선언
+	number = 5; //변수값설정
+	char ascii = '5'; //변수초기화
+
+	int num = 312;
+	printf("num의 값은 %d입니다.", num); //변수값 출력 -> num의 값은 312입니다.
+	
+
+}
